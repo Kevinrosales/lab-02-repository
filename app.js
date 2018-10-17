9b4220025688674661d8ac1e03384568b5bcc189
Kevinrosales <kevinrosalesolvera@gmail.com>
2018-10-17 10:33:02 -0700

    created constructor and render function

---
+++ app.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const allHorns = [];
+
+function Horn (hornObject) {
+    this.image_url = hornObject.image_url;
+    this.title = hornObject.title;
+    this.description = hornObject.description;
+    this.keyword = hornObject.keyword;
+    this.horns = hornObject.horns;
+
+    allHorns.push(this);
+}
+
+Horn.prototype.render = function(){
+    $('main').append('<div class = "newAnimal"></div>');
+    let $hornContainer = $('div[class = "clone"]');
+    console.log($hornContainer);
+
+    let $templateHTML = $('#photo-template').html();
+    console.log($templateHTML);
+    $hornContainer.html($templateHTML);
+
+    $hornContainer.find('h2').text(this.title);
+    $hornContainer.find('img').attr('src',this.image_url);
+    $hornContainer.find('p').text(this.description);
+
+    $hornContainer.attr('class', '');
+}
+
+const readJSON = function(){
+    $.get('./page-1.json', data => {
+      data.forEach(animal => {
+  
+        new Horn(animal);
+  
+      })
+    })//.then(render)
+
+}
+console.log(allHorns);
+readJSON();
